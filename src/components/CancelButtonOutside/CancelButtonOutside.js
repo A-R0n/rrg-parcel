3bcdc665e6cdba66ec6f112f184ca6745d993894
Aaron Estes <aestesc@gmail.com>
2021-02-25 23:24:39 -0600

    change div to button

--- src/components/CancelButtonOutside/CancelButtonOutside.js
+++ src/components/CancelButtonOutside/CancelButtonOutside.js
@@ -3,14 +3,14 @@ import React from "react";
 export const CancelButtonOutside = (props) => {
   return (
     <div className="cancel-btn-holder">
-      <div
+      <button
         className="cancel-btn"
         type="button"
         onClick={() => props.toggleThat()}
         style={{marginLeft: "10px", height: "4.5vh"}}
       >
         Cancel
-      </div>
+      </button>
     </div>
   );
 };
