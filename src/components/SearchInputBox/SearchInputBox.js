dc51c6b29947af8db0cc5cb1b94e07d47b3b1ecf
Aaron Estes <aestesc@gmail.com>
2021-03-01 13:52:07 -0600

    change type to int for max length

--- src/components/SearchInputBox/SearchInputBox.js
+++ src/components/SearchInputBox/SearchInputBox.js
@@ -64,7 +64,6 @@ export const SearchInputBox = (props) => {
             value={routeName}
             onChange={handleUserTyping}
             placeholder="Search for a route"
-            maxLength={20}
             // onClick={clickInInput}
             // ref={comboBoxRef}
           />
